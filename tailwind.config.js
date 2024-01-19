9b043ffb35e38bcbfdde4ecdb90385173787aa7c
murtazarangwala99 <murtazarangwala95@gmail.com>
2024-01-19 11:04:06 +0530

    added postcss for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["*"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        beige: "#ad9970",
+      },
+    },
   },
   plugins: [],
-}
+};
